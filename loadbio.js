6717c10066430d970f3196d3ea0626ad95688dd5
Sabrina L <sabrinali0312@gmail.com>
2019-02-09 13:00:42 -0500

    responsive header/nav

---
+++ loadbio.js
@@ -0,0 +1,24 @@
+function protClicked(){
+    $("#bio-main").load("portfolio.html"); 
+    var x = document.getElementById("mynav");
+    x.className = "nav";
+    
+}
+
+function contactClicked(){
+    $("#bio-main").load("contact.html"); 
+    var x = document.getElementById("mynav");
+    x.className = "nav";
+    
+}
+
+        
+
+function showVerticalNav() {
+    var x = document.getElementById("mynav");
+    if (x.className === "nav") {
+    x.className += " responsive";
+    } else {
+    x.className = "nav";
+    }
+}
