8a4215959658f4d58f0accf999115730615a7088
Sabrina L <sabrinali0312@gmail.com>
2019-04-07 12:50:01 -0400

    xray animation

--- loadbio.js
+++ loadbio.js
@@ -1,15 +1,17 @@
-function protClicked(){
-    $("#bio-main").load("portfolio.html"); 
-    var x = document.getElementById("mynav");
-    x.className = "nav";
-    
-}
+// function protClicked(){
+//     $("#bio-main").load("portfolio.html"); 
+//     var x = document.getElementById("mynav");
+//     x.className = "nav";
+//     console.log("here")
+//     $("#square").mouseover(function(event){
+//         console.log(event);
+//     } )
+// }
 
 function contactClicked(){
     $("#bio-main").load("contact.html"); 
     var x = document.getElementById("mynav");
     x.className = "nav";
-    
 }
 
 function showVerticalNav() {
@@ -22,3 +24,18 @@ function showVerticalNav() {
 }
 
 
+$(document).ready(function(){
+    $(".square").mousemove(function(event){
+        $($("#clipping-1").children("circle")).attr("cx",event.offsetX);
+        $($("#clipping-1").children("circle")).attr("cy",event.offsetY);
+    })
+    $(".square").mouseover(function(event){
+        // $($(this).children("a")[0]).children("img").css({"clip-path": ""})
+        $($(this).children("a")[0]).children("img").addClass("svgclipping")
+    })
+    $(".square").mouseout(function(event){
+        // $($(this).children("a")[0]).children("img").css({"clip-path": ""})
+        $($(this).children("a")[0]).children("img").removeClass("svgclipping")
+    })
+
+})
