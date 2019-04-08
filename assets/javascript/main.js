a04ef9b00a5a7646580cb10e6df46eed7ca9675c
Sabrina L <sabrinali0312@gmail.com>
2019-04-08 16:22:13 -0400

    responsive ness

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,14 +7,23 @@ $(document).ready(function(){
     })
     $(".square").mouseover(function(event){
         // $($(this).children("a")[0]).children("img").css({"clip-path": ""})
-        $($(this).children("a")[0]).children("img").addClass("svgclipping")
+        $(this).children("img").addClass("svgclipping")
     })
     $(".square").mouseout(function(event){
         // $($(this).children("a")[0]).children("img").css({"clip-path": ""})
-        $($(this).children("a")[0]).children("img").removeClass("svgclipping")
+        $(this).children("img").removeClass("svgclipping")
     })
 
-    $("#verticalNav").on('clicken',showVerticalNav);
+    $("#verticalNav").on('click',showVerticalNav);
+    $(".square").on('click',function(e){
+        //TODO:detect touch screen
+        // if ($(this).attr("data-lifted")){
+            var win = window.open("https://sabrina-li.github.io/Word-Guess-Game", '_blank');
+            win.focus();
+        // }else{
+        //     $(this).attr("data-lifted",true);
+        // }
+    })
 })
 
 
@@ -26,10 +35,11 @@ function contactClicked(){
 }
 
 function showVerticalNav() {
-    var x = document.getElementById("mynav");
-    if (x.className === "nav") {
-    x.className += " responsive";
-    } else {
-    x.className = "nav";
-    }
+    $("#h1header").toggleClass("responsive");
+    $("#mynav").toggleClass("responsive");
+    // if (x.className === "nav") {
+    // x.className += " responsive";
+    // } else {
+    // x.className = "nav";
+    // }
 }
